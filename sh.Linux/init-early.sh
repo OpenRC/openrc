53097aedc57aafb174e32cbeb8f6527d150e2a66
Roy Marples <roy@marples.name>
2007-07-01 08:45:23 +0000

    Don't set an early font if consolefont isn't in a runlevel, #183773.

--- sh.Linux/init-early.sh
+++ sh.Linux/init-early.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 # Try and set a font and as early as we can
-termencoding="(K"
-[ -e "${RC_LIBDIR}"/console/unicode ] && termencoding="%G"
-printf "\033%s" "${termencoding}"
-if [ -r "${RC_LIBDIR}"/console/font -a -x /bin/setfont ] ; then
-	font="$(cat "${RC_LIBDIR}"/console/font)"
-	CONSOLE="${CONSOLE:-/dev/console}"
-	[ -c "${CONSOLE}" ] && cons="-C ${CONSOLE}"
-	setfont ${cons} "${RC_LIBDIR}"/console/"${font}"
+if [ -e /etc/runlevels/"${RC_DEFAULTLEVEL}"/consolefont \
+	 -o -e /etc/runlevels/"${RC_BOOTLEVEL}"/consolefont ] ; then
+	termencoding="(K"
+	[ -e "${RC_LIBDIR}"/console/unicode ] && termencoding="%G"
+	printf "\033%s" "${termencoding}"
+	if [ -r "${RC_LIBDIR}"/console/font -a -x /bin/setfont ] ; then
+		font="$(cat "${RC_LIBDIR}"/console/font)"
+		CONSOLE="${CONSOLE:-/dev/console}"
+		[ -c "${CONSOLE}" ] && cons="-C ${CONSOLE}"
+		setfont ${cons} "${RC_LIBDIR}"/console/"${font}"
+	fi
 fi
