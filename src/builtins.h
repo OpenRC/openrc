e6ec76ac40757d51de8d9016b5214014c5c500ae
Roy Marples <roy@marples.name>
2007-07-31 16:06:21 +0000

    All our binaries are now mulicalls into rc, which makes our on disk size a lot smaller.

---
+++ src/builtins.h
@@ -0,0 +1,17 @@
+/*
+ * @file builtins.h 
+ * @brief declaration for our userland helpers to be built into rc
+ * @internal
+ *
+ * Copyright 2007 Gentoo Foundation
+ * Released under the GPLv2
+ */
+
+int env_update (int argc, char **argv);
+int fstabinfo (int argc, char **argv);
+int mountinfo (int argc, char **argv);
+int rc_depend (int argc, char **argv);
+int rc_status (int argc, char **argv);
+int runscript (int argc, char **argv);
+int start_stop_daemon (int argc, char **argv);
+
