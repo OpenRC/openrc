1d0b234e942bbd5572585abf2b156e9991057cd4
Roy Marples <roy@marples.name>
2007-09-26 10:46:44 +0000

    Fix Makefile for pmake, document s-s-d options and increase usage gap

--- src/_usage.c
+++ src/_usage.c
@@ -20,7 +20,7 @@ static void usage (int exit_status)
 	for (i = 0; longopts[i].name; ++i) {
 		int len = printf ("  -%c, --%s %s", longopts[i].val, longopts[i].name,
 		                  has_arg[longopts[i].has_arg]);
-		while (++len < 25)
+		while (++len < 30)
 			printf (" ");
 		puts (longopts_help[i]);
 	}
