d063cda7e1afd959dea908bf8bb892ce7db7fe40
Roy Marples <roy@marples.name>
2007-11-20 11:59:44 +0000

    Move the runlevel foo to it's own Makefile for ease of maintainance

---
+++ runlevels.mk
@@ -0,0 +1,18 @@
+# Handy runlevel stuff
+
+LEVELDIR = $(DESTDIR)/etc/runlevels
+BOOTDIR = $(LEVELDIR)/boot
+DEFAULTDIR = $(LEVELDIR)/default
+
+install:
+	if ! test -d "$(BOOTDIR)" ; then \
+		$(INSTALL) -d $(BOOTDIR) || exit $$?; \
+		for x in $(BOOT); do ln -snf ../../init.d/"$$x" $(BOOTDIR)/"$$x" || exit $$? ; done \
+	fi
+	if ! test -d "$(DEFAULTDIR)" ; then \
+		$(INSTALL) -d $(DEFAULTDIR) || exit $$?; \
+		for x in $(DEFAULT); do ln -snf ../../init.d/"$$x" $(DEFAULTDIR)/"$$x" || exit $$? ; done \
+	fi
+
+all:
+clean:
