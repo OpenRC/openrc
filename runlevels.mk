d5684c26f9f51f2db3f5d586989cfb40f0b0318b
Roy Marples <roy@marples.name>
2007-12-28 11:59:42 +0000

    runlevel links are not relavtive, so don't install defaults as such.

--- runlevels.mk
+++ runlevels.mk
@@ -9,11 +9,11 @@ INSTALL ?= install
 install:
 	if ! test -d "$(BOOTDIR)"; then \
 		$(INSTALL) -d $(BOOTDIR) || exit $$?; \
-		for x in $(BOOT); do ln -snf ../../init.d/"$$x" $(BOOTDIR)/"$$x" || exit $$?; done \
+		for x in $(BOOT); do ln -snf /etc/init.d/"$$x" $(BOOTDIR)/"$$x" || exit $$?; done \
 	fi
 	if ! test -d "$(DEFAULTDIR)"; then \
 		$(INSTALL) -d $(DEFAULTDIR) || exit $$?; \
-		for x in $(DEFAULT); do ln -snf ../../init.d/"$$x" $(DEFAULTDIR)/"$$x" || exit $$?; done \
+		for x in $(DEFAULT); do ln -snf /etc/init.d/"$$x" $(DEFAULTDIR)/"$$x" || exit $$?; done \
 	fi
 
 all:
