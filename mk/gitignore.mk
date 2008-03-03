b614477948cfe21185f2a07f7b0d2b994371e876
Roy Marples <roy@marples.name>
2008-03-03 13:31:25 +0000

    Fix ignore files some more.

--- mk/gitignore.mk
+++ mk/gitignore.mk
@@ -6,13 +6,9 @@ IGNOREFILES+=	${CLEANFILES}
 .PHONY:		.gitignore
 
 .gitignore:
-	@if test -n "${IGNOREFILES}"; then echo "Ignoring ${IGNOREFILES}"; fi
-	@for obj in ${IGNOREFILES}; do \
-		if ! test -r .gitignore; then \
-			echo "$${obj}" > .gitignore || exit $$?; \
-		elif ! grep -q "^$${obj}$$" .gitignore; then \
-			echo "$${obj}" >> .gitignore || exit $$?; \
-		fi; \
-	done
+	@if test -n "${IGNOREFILES}"; then \
+		echo "Ignoring ${IGNOREFILES}"; \
+		echo ${IGNOREFILES} | tr ' ' '\n' > .gitignore; \
+	fi
 
 gitignore: .gitignore
