a718975e080b04163b181775a8b5fa5f47f0c048
Roy Marples <roy@marples.name>
2008-03-26 17:55:35 +0000

    Add license blurb to a few files and append my email to my name.

--- mk/os-Linux.mk
+++ mk/os-Linux.mk
@@ -1,4 +1,5 @@
-# Copyright 2008 Roy Marples
+# Copyright 2008 Roy Marples <roy@marples.name>
+# All rights reserved. Released under the 2-clause BSD license.
 
 CFLAGS+=	-D_BSD_SOURCE -D_XOPEN_SOURCE=600
 LIBDL=		-Wl,-Bdynamic -ldl
