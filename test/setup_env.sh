d61f8318962346b3b2a3f13e7b489d2c0c397b94
Roy Marples <roy@marples.name>
2008-07-03 13:09:24 +0000

    More unit fixes.

--- test/setup_env.sh
+++ test/setup_env.sh
@@ -9,6 +9,11 @@ srcdir=${srcdir:-.}
 top_builddir=${top_builddir:-${top_srcdir}}
 builddir=${builddir:-${srcdir}}
 
+if ! . ${top_srcdir}/sh/functions.sh; then
+	echo "Sourcing functions.sh failed !?" 1>&2
+	exit 1
+fi
+
 export LD_LIBRARY_PATH=${top_builddir}/src/libeinfo:${top_builddir}/src/librc:${LD_LIBRARY_PATH}
 export PATH=${top_builddir}/src/rc:${PATH}
 
@@ -16,9 +21,3 @@ cd ${top_srcdir}/src/rc
 ${MAKE:-make} links >/dev/null
 cd -
 
-. ${top_srcdir}/sh/functions.sh
-
-if [ $? -ne 0 ] ; then
-	echo "Sourcing functions.sh failed !?" 1>&2
-	exit 1
-fi
