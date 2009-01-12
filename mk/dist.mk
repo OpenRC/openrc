2243c013900f1d3d850acf066dde2e83f41b3ed9
Roy Marples <roy@marples.name>
2009-01-12 23:12:06 +0000

    Add snapshot and snap targets

--- mk/dist.mk
+++ mk/dist.mk
@@ -5,9 +5,28 @@
 DISTPREFIX?=	${NAME}-${VERSION}
 DISTFILE?=	${DISTPREFIX}.tar.bz2
 
-CLEANFILES+=	${DISTFILE}
+CLEANFILES+=	${NAME}-*.tar.bz2
+
+_SNAP_SH=	date -u +%Y%m%d%H%M
+_SNAP!=		${_SNAP_SH}
+SNAP=		${_SNAP}$(shell ${_SNAP_SH})
+SNAPDIR=	${DISTPREFIX}-${SNAP}
+SNAPFILE=	${SNAPDIR}.tar.bz2
 
 dist:
 	svn export . ${DISTPREFIX}
 	tar cjpf ${DISTFILE} ${DISTPREFIX}
 	rm -rf ${DISTPREFIX}
+
+snapshot:
+	rm -rf /tmp/${SNAPDIR}
+	mkdir /tmp/${SNAPDIR}
+	cp -RPp * /tmp/${SNAPDIR}
+	(cd /tmp/${SNAPDIR}; make clean)
+	find /tmp/${SNAPDIR} -name .svn -exec rm -rf -- {} \; 2>/dev/null || true
+	tar -cvjpf ${SNAPFILE} -C /tmp ${SNAPDIR}
+	rm -rf /tmp/${SNAPDIR}
+	ls -l ${SNAPFILE}
+
+snap: snapshot
+
