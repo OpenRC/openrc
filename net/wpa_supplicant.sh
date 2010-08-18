1a188f8f725186b2c2d6d7c67e79c2ff5ea9d05e
William Hubbs <williamh@gentoo.org>
2010-08-18 12:22:15 -0500

    fix bug 266395
    Do not stop wpa_supplicant or wpa_cli if they are in the background.
    Thanks to Slava Gorbunov for the patch.

--- net/wpa_supplicant.sh
+++ net/wpa_supplicant.sh
@@ -183,9 +183,11 @@ wpa_supplicant_post_stop()
 	fi
 
 	if yesno "${IN_BACKGROUND}"; then
-		# Only stop wpa_supplicant if it's not the controlling daemon
-		! service_started_daemon "${RC_SVCNAME}" "${wpas}" 1
+		# Don't stop wpa_supplicant & wpa_cli if in background
+		return 0
 	fi
+	# Only stop wpa_supplicant if it's not the controlling daemon
+	! service_started_daemon "${RC_SVCNAME}" "${wpas}" 1
 	[ $? != 0 ] && return 0
 
 	local pidfile="/var/run/wpa_cli-${IFACE}.pid"
