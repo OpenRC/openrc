694fd2027f693fc94c84b0025b36a5f4f90d9ac1
Brant Gurganus <brant@gurganus.name>
2010-10-24 13:08:38 -0400

    correction for unmounting volumes with weird characters

--- sh/rc-mount.sh
+++ sh/rc-mount.sh
@@ -26,6 +26,7 @@ do_unmount()
 		# Unmounting a shared mount can unmount other mounts, so
 		# we need to check the mount is still valid
 		mountinfo --quiet "$mnt" || continue
+		mnt=$(printf "$mnt")
 
 		case "$cmd" in
 			umount)
