eebb2d1b9cf0da22ed654ca7ac97a0ae845802ba
Robin H. Johnson <robbat2@gentoo.org>
2011-01-18 01:49:22 +0000

    test: You need to run "make" before "make check" will work.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- test/setup_env.sh
+++ test/setup_env.sh
@@ -12,7 +12,11 @@ builddir=${builddir:-${srcdir}}
 export LD_LIBRARY_PATH=${top_builddir}/src/libeinfo:${top_builddir}/src/librc:${LD_LIBRARY_PATH}
 export PATH=${top_builddir}/src/rc:${PATH}
 
-if ! . ${top_srcdir}/sh/functions.sh; then
+
+if [ ! -f ${top_srcdir}/sh/functions.sh ] ; then
+	echo "functions.sh not yet created !?" 1>&2
+	exit 1
+elif ! . ${top_srcdir}/sh/functions.sh; then
 	echo "Sourcing functions.sh failed !?" 1>&2
 	exit 1
 fi
