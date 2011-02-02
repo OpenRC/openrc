a2e9cde737ede93502a80fe50e62b0aea50643dc
William Hubbs <williamh@gentoo.org>
2011-02-02 16:42:17 -0600

    remove BSD make code to handle including .depend

--- mk/depend-gmake.mk
+++
@@ -1,3 +0,0 @@
-# Tell gmake to include the optional dependency file.
-# This sucks, but I don't know any other way of portably making this work.
--include .depend
--- mk/depend.mk
+++ mk/depend.mk
@@ -10,9 +10,4 @@ IGNOREFILES+=	.depend
 
 depend: .depend extra_depend
 
-# Nasty hack. depend-.mk is a blank file, depend-gmake.mk has a gmake specific
-# command to optionally include .depend.
-# Someone should patch gmake to optionally include .depend if it exists.
-_INC_DEP=	$(shell if ${MAKE} --version | grep -q "^GNU "; then \
-		echo "gmake"; else echo ""; fi)
-include ${MK}/depend-${_INC_DEP}.mk
+-include .depend
