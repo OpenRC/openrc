db6d680765b4b9a4d404f6b2e6d5644b2fe3418d
William Hubbs <w.d.hubbs@gmail.com>
2011-02-02 18:18:55 -0600

    rework pam, ncurses and termcap include files
    This allows error checking inside the included files instead of either
    including the appropriate file or a blank file.
    
    Also the blank file named .mk gets removed by this change.

--- mk/pam.mk
+++ mk/pam.mk
@@ -1,6 +1,10 @@
+ifeq (${MKPAM},pam)
 LIBPAM?=	-lpam
 CPPFLAGS+=	-DHAVE_PAM
 LDADD+=		${LIBPAM}
 
 PAMDIR?=	/etc/pam.d
 PAMMODE?=	0644
+else ifneq (${MKPAM},)
+$(error if MKPAM is defined, it must be "pam")
+endif
--- mk/termcap.mk
+++ mk/termcap.mk
@@ -1,3 +1,11 @@
+ifeq (${MKTERMCAP},ncurses)
+LIBTERMCAP?=	-lncurses
+CPPFLAGS+=	-DHAVE_TERMCAP
+LDADD+=		${LIBTERMCAP}
+else ifeq (${MKTERMCAP},termcap)
 LIBTERMCAP?=	-ltermcap
 CPPFLAGS+=	-DHAVE_TERMCAP
 LDADD+=		${LIBTERMCAP}
+else ifneq (${MKTERMCAP},)
+$(error If MKTERMCAP is defined, it must be ncurses or termcap)
+endif
