5b1aaf8cc803e9f21f8a0c94236ab5dddba4b2fc
William Hubbs <williamh@gentoo.org>
2011-10-17 13:47:45 -0500

    add support for extra_stopped_commands

--- src/librc/librc.c
+++ src/librc/librc.c
@@ -526,7 +526,9 @@ rc_service_exists(const char *service)
 }
 librc_hidden_def(rc_service_exists)
 
-#define OPTSTR ". '%s'; echo $extra_commands $extra_started_commands"
+#define OPTSTR \
+". '%s'; echo $extra_commands $extra_started_commands $extra_stopped_commands"
+
 RC_STRINGLIST *
 rc_service_extra_commands(const char *service)
 {
