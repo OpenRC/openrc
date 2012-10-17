21abe9821cfad8f106761ebaca0b627bac917a2f
William Hubbs <w.d.hubbs@gmail.com>
2012-10-17 18:13:47 -0500

    tests: fix the paths
    When the test suite is being run, we need our local directories in PATH
    and LD_LIBRARY_PATH before the system directories. This makes sure we
    run our tests using the currently built tree.

--- test/setup_env.sh
+++ test/setup_env.sh
@@ -9,10 +9,6 @@ srcdir=${srcdir:-.}
 top_builddir=${top_builddir:-${top_srcdir}}
 builddir=${builddir:-${srcdir}}
 
-export LD_LIBRARY_PATH=${top_builddir}/src/libeinfo:${top_builddir}/src/librc:${LD_LIBRARY_PATH}
-export PATH=${top_builddir}/src/rc:${PATH}
-
-
 if [ ! -f ${top_srcdir}/sh/functions.sh ] ; then
 	echo "functions.sh not yet created !?" 1>&2
 	exit 1
@@ -20,6 +16,9 @@ elif ! . ${top_srcdir}/sh/functions.sh; then
 	echo "Sourcing functions.sh failed !?" 1>&2
 	exit 1
 fi
+
+export LD_LIBRARY_PATH=${top_builddir}/src/libeinfo:${top_builddir}/src/librc:${LD_LIBRARY_PATH}
+export PATH=${top_builddir}/src/rc:${PATH}
 
 cd ${top_srcdir}/src/rc
 ${MAKE:-make} links >/dev/null
