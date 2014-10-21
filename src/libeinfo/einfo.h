f77006b58e3fffb80208804dae75a69a29414e95
Anthony G. Basile <blueness@gentoo.org>
2014-10-21 09:31:07 -0400

    einfo.h, rc.h.in: ensure __BEGIN_DECLS is defined
    Some Standard C Libraries, like musl, don't define __BEGIN_DECLS
    or __END_DECLS.  We add some ifdef magic to ensure these are
    available.

--- src/libeinfo/einfo.h
+++ src/libeinfo/einfo.h
@@ -48,6 +48,16 @@
 # endif
 #endif
 
+#undef __BEGIN_DECLS
+#undef __END_DECLS
+#ifdef __cplusplus
+# define __BEGIN_DECLS extern "C" {
+# define __END_DECLS }
+#else
+# define __BEGIN_DECLS /* empty */
+# define __END_DECLS /* empty */
+#endif
+
 __BEGIN_DECLS
 
 /*! @brief Color types to use */
