ba0a11fc94d303e208dd364b06c1c2a75bcdd62b
Johan Bergström <bugs@bergstroem.nu>
2014-10-23 09:59:57 +1100

    Pass ncurses cflags to build
    Fixes #25
    Note from William Hubbs:
    The original patch overwrote CFLAGS. I modified this patch to add the
    ncurses cflags to CPPFLAGS instead of overwriting CFLAGS.

--- mk/termcap.mk
+++ mk/termcap.mk
@@ -1,11 +1,12 @@
 ifeq (${MKTERMCAP},ncurses)
-	LTERMCAP:=	$(shell pkg-config ncurses --libs 2> /dev/null)
+TERMCAP_CFLAGS:=	$(shell pkg-config ncurses --cflags 2> /dev/null)
+LTERMCAP:=			$(shell pkg-config ncurses --libs 2> /dev/null)
 ifeq ($(LTERMCAP),)
 LIBTERMCAP?=	-lncurses
 else
 LIBTERMCAP?= $(LTERMCAP)
 endif
-CPPFLAGS+=	-DHAVE_TERMCAP
+CPPFLAGS+=	-DHAVE_TERMCAP ${TERMCAP_CFLAGS}
 LDADD+=		${LIBTERMCAP}
 else ifeq (${MKTERMCAP},termcap)
 LIBTERMCAP?=	-ltermcap
