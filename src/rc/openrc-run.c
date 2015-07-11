5a2ed6874e10a8fb1b0053a683be9f2aed5284f4
Mike Gilbert <floppym@gentoo.org>
2015-07-11 16:19:08 -0400

    openrc-run: Fix default behavior when rc_hotplug is defined
    When rc_hotplug is undefined, service_pluggable always returns false.
    
    When rc_hotplug is defined, service_pluggable previously would return
    true unless an entry in rc_hotplug negated the behavior for a specific
    service.
    
    This change makes service_pluggable return false unless an entry in
    rc_hotplug says otherwise.

--- src/rc/openrc-run.c
+++ src/rc/openrc-run.c
@@ -1028,7 +1028,7 @@ static bool
 service_plugable(void)
 {
 	char *list, *p, *token;
-	bool allow = true, truefalse;
+	bool allow = false, truefalse;
 	char *match = rc_conf_value("rc_hotplug");
 
 	if (!match)
