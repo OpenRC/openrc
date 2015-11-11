OpenRC/openrc

commit 3f82edbeb9251149c6aff071d6537379af4e5eea

Fix GNU/kFreeBSD port
Check for __FreeBSD_kernel instead of __GLIBC__ in source files.

note from William Hubbs:
I was told this is a better check for GNU/kFreeBSD than checking the
C  library the source is being compiled against.
GNU/kFreeBSD than checking which library we are using.

src/librc/librc-daemon.c
@@ -30,7 +30,7 @@
 
 #include "librc.h"
 
-#if defined(__linux__) || defined (__GLIBC__)
+#if defined(__linux__) || defined (__FreeBSD_kernel__)
 static bool
 pid_is_exec(pid_t pid, const char *exec)
 {

src/rc/mountinfo.c
@@ -39,7 +39,7 @@
 #  include <sys/statvfs.h>
 #  define statfs statvfs
 #  define F_FLAGS f_flag
-#elif defined (__linux__) || defined (__GLIBC__)
+#elif defined (__linux__) || defined (__FreeBSD_kernel__)
 #  include <mntent.h>
 #endif
 
@@ -265,7 +265,7 @@ find_mounts(struct args *args)
 	return list;
 }
 
-#elif defined (__linux__) || defined (__GLIBC__)
+#elif defined (__linux__) || defined (__FreeBSD_kernel__)
 static struct mntent *
 getmntfile(const char *file)
 {

src/rc/rc-logger.c
@@ -44,7 +44,7 @@
 #include <time.h>
 #include <unistd.h>
 
-#if defined(__linux__) || defined(__GLIBC__)
+#if defined(__linux__) || defined(__FreeBSD_kernel__)
 #  include <pty.h>
 #elif defined(__NetBSD__) || defined(__OpenBSD__)
 #  include <util.h>

src/rc/runscript.c
@@ -51,7 +51,7 @@
 #include <time.h>
 #include <unistd.h>
 
-#if defined(__linux__) || defined(__GLIBC__)
+#if defined(__linux__) || defined(__FreeBSD_kernel__)
 #  include <pty.h>
 #elif defined(__NetBSD__) || defined(__OpenBSD__)
 #  include <util.h>

commit 085d77f17e3bedd23ffa96fe7e4eb8515ae8bfc6

Standardize macro tests for gnu hurd
This also fixes breakage of GNU/hurd builds introduced by commit 3f82edbeb92.

src/librc/librc-daemon.c
@@ -31,7 +31,8 @@
 #include "queue.h"
 #include "librc.h"
 
-#if defined(__linux__) || (defined (__FreeBSD_kernel__) && defined(__GLIBC__))
+#if defined(__linux__) || (defined (__FreeBSD_kernel__) && defined(__GLIBC__)) \
+	|| defined(__GNU__)
 static bool
 pid_is_exec(pid_t pid, const char *exec)
 {

src/rc/mountinfo.c
@@ -39,8 +39,8 @@
 #  include <sys/statvfs.h>
 #  define statfs statvfs
 #  define F_FLAGS f_flag
-#elif defined (__linux__) || (defined(__FreeBSD_kernel__) && \
-		defined(__GLIBC__)) || defined(__GNU__)
+#elif defined(__linux__) || (defined(__FreeBSD_kernel__) && \
+	defined(__GLIBC__)) || defined(__GNU__)
 #  include <mntent.h>
 #endif
 
@@ -267,8 +267,8 @@ find_mounts(struct args *args)
 	return list;
 }
 
-#elif defined (__linux__) || (defined (__FreeBSD_kernel__) && \
-		defined(__GLIBC__))
+#elif defined(__linux__) || (defined(__FreeBSD_kernel__) && \
+	defined(__GLIBC__)) || defined(__GNU__)
 static struct mntent *
 getmntfile(const char *file)
 {

src/rc/openrc-run.c
@@ -51,8 +51,8 @@
 #include <time.h>
 #include <unistd.h>
 
-#if defined(__linux__) || (defined(__FreeBSD_kernel__) && \
-		defined(__GLIBC__))
+#if defined(__linux__) || (defined(__FreeBSD_kernel__) && defined(__GLIBC__)) \
+	|| defined(__GNU__)
 #  include <pty.h>
 #elif defined(__NetBSD__) || defined(__OpenBSD__)
 #  include <util.h>

src/rc/rc-logger.c
@@ -44,7 +44,8 @@
 #include <time.h>
 #include <unistd.h>
 
-#if defined(__linux__) || (defined(__FreeBSD_kernel__) && defined(__GLIBC__))
+#if defined(__linux__) || (defined(__FreeBSD_kernel__) && defined(__GLIBC__)) \
+	|| defined(__GNU__)
 #  include <pty.h>
 #elif defined(__NetBSD__) || defined(__OpenBSD__)
 #  include <util.h>