2b1392af2fe9e5dfc8eda2f19d896efdc41840bf
William Hubbs <w.d.hubbs@gmail.com>
2018-05-09 17:30:08 -0500

    Logger: only log printable characters and newlines
    X-Gentoo-Bug: 651412
    X-Gentoo-Bug-URL: https://bugs.gentoo.org/show_bug.cgi?id=651412

--- src/rc/rc-logger.c
+++ src/rc/rc-logger.c
@@ -87,8 +87,9 @@ write_log(int logfd, const char *buffer, size_t bytes)
 		}
 
 		if (!in_escape) {
-			if (write(logfd, p++, 1) == -1)
-				eerror("write: %s", strerror(errno));
+			if (isprint((unsigned char) *p) || *p == '\n')
+				if (write(logfd, p++, 1) == -1)
+					eerror("write: %s", strerror(errno));
 			continue;
 		}
 
