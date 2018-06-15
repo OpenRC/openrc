a097933edab9d7221df1831e098415d604437c98
William Hubbs <w.d.hubbs@gmail.com>
2018-06-15 17:44:39 -0500

    sh/start-stop-daemon.sh: fix processing of the logger arguments

--- sh/start-stop-daemon.sh
+++ sh/start-stop-daemon.sh
@@ -38,6 +38,10 @@ ssd_start()
 		service_inactive && _inactive=true
 		mark_service_inactive
 	fi
+	[ -n "$output_logger" ] && 
+		output_logger_arg="--stdout-logger \"$output_logger\""
+	[ -n "$error_logger" ] && 
+		error_logger_arg="--stderr-logger \"$error_logger\""
 	#the eval call is necessary for cases like:
 	# command_args="this \"is a\" test"
 	# to work properly.
@@ -47,8 +51,8 @@ ssd_start()
 		${directory:+--chdir} $directory \
 		${output_log+--stdout} $output_log \
 		${error_log+--stderr} $error_log \
-		${output_logger+--stdout-logger} \"$output_logger\" \
-		${error_logger+--stderr-logger} \"$error_logger\" \
+		${output_logger_arg} \
+		${error_logger_arg} \
 		${procname:+--name} $procname \
 		${pidfile:+--pidfile} $pidfile \
 		${command_user+--user} $command_user \
