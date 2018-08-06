OpenRC/openrc

commit 37e29442721af0dc3846e87ef2b85a474af3cf2b

librc: Add check for crashed state
In rc_service_state,, call rc_service_daemons_crashed to check for
a crashed daemon if the service is started.

src/librc/librc.c
@@ -849,6 +849,10 @@ rc_service_state(const char *service)
 		}
 	}
 
+	if (state & RC_SERVICE_STARTED) {
+		if (rc_service_daemons_crashed(service))
+			state |= RC_SERVICE_CRASHED;
+	}
 	if (state & RC_SERVICE_STOPPED) {
 		dirs = ls_dir(RC_SVCDIR "/scheduled", 0);
 		TAILQ_FOREACH(dir, dirs, entries) {

commit 69f1540d8e8e3750b8e8e1c6b2842a1881a0c537

librc: fix EACCES errno false-positive crash
Use errno != EACCES to fix false-positive for non-root users
with grsecurity kernels.

Fixes: 37e29442721a ("librc: Add check for crashed state")

src/librc/librc.c
@@ -850,7 +850,7 @@ rc_service_state(const char *service)
 	}
 
 	if (state & RC_SERVICE_STARTED) {
-		if (rc_service_daemons_crashed(service))
+		if (rc_service_daemons_crashed(service) && errno != EACCES)
 			state |= RC_SERVICE_CRASHED;
 	}
 	if (state & RC_SERVICE_STOPPED) {