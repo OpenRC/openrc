2db9ffe248911ec6bf5302a1bddce1c1e79a9024
Austin English <austinenglish@gmail.com>
2018-10-12 17:20:58 -0500

    give TravisCI a try

---
+++ test/travis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) 2007-2018 The OpenRC Authors.
+# See the Authors file at the top-level directory of this distribution and
+# https://github.com/OpenRC/openrc/blob/master/AUTHORS
+#
+# This file is part of OpenRC. It is subject to the license terms in
+# the LICENSE file found in the top-level directory of this
+# distribution and at https://github.com/OpenRC/openrc/blob/master/LICENSE
+# This file may not be copied, modified, propagated, or distributed
+# except according to the terms contained in the LICENSE file.
+
+set -e
+set -u
+set -x
+
+# These are steps to run on TravisCI under a containerized Ubuntu system.
+# See $TOP/.travis.yml for more info about the TravisCI setup.
+
+cpus=$(getconf _NPROCESSORS_CONF || echo 1)
+# make on TravisCI doesn't support -O yet
+make -j"${cpus}"
+
+# This make whitespace/etc. checks fatal.
+export FATAL_CHECKS=1
+make test
