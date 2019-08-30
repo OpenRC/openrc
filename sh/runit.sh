bcf4be73b380ff3574bf76b48db20c8494cb19b9
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2019-08-30 21:37:46 +0530

    remove extra check and extra time option

--- sh/runit.sh
+++ sh/runit.sh
@@ -21,15 +21,15 @@ runit_start()
 	ebegin "Starting ${name:-$RC_SVCNAME}"
 	ln -snf "${service_path}" "${service_link}"
 	local i=0 retval=1
+	# it can take upto 5 seconds for runsv to start
 	while [ $i -lt 6 ] ; do
-		if sv -w 6 start "${service_link}" > /dev/null 2>&1; then
+		if sv start "${service_link}" > /dev/null 2>&1; then
 			retval=0
 			break
 		fi
 		sleep 1 && i=$(expr $i + 1)
 	done
-	sv start "${service_link}" > /dev/null 2>&1
-	eend $? "Failed to start ${name:-$RC_SVCNAME}"
+	eend $retval "Failed to start ${name:-$RC_SVCNAME}"
 }
 
 runit_stop()
