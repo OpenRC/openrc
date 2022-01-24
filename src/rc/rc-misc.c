OpenRC/openrc

commit 8e02406d8fbf92167c30431987d5de8de72cd7df

rc-misc.c: remove references to PATH_MAX

src/rc/rc-misc.c
@@ -131,7 +131,8 @@ env_config(void)
 	char *npp;
 	char *tok;
 	const char *sys = rc_sys();
-	char buffer[PATH_MAX];
+	char *buffer = NULL;
+	size_t size = 0;
 
 	/* Ensure our PATH is prefixed with the system locations first
 	   for a little extra security */
@@ -170,8 +171,7 @@ env_config(void)
 	free(e);
 
 	if ((fp = fopen(RC_KRUNLEVEL, "r"))) {
-		memset(buffer, 0, sizeof (buffer));
-		if (fgets(buffer, sizeof (buffer), fp)) {
+		if (getline(&buffer, &size, fp) != -1) {
 			l = strlen (buffer) - 1;
 			if (buffer[l] == '\n')
 				buffer[l] = 0;
@@ -181,6 +181,7 @@ env_config(void)
 	} else
 		setenv("RC_DEFAULTLEVEL", RC_LEVEL_DEFAULT, 1);
 
+	free(buffer);
 	if (sys)
 		setenv("RC_SYS", sys, 1);
 
@@ -232,11 +233,12 @@ signal_setup_restart(int sig, void (*handler)(int))
 int
 svc_lock(const char *applet)
 {
-	char file[PATH_MAX];
+	char *file = NULL;
 	int fd;
 
-	snprintf(file, sizeof(file), RC_SVCDIR "/exclusive/%s", applet);
+	xasprintf(&file, RC_SVCDIR "/exclusive/%s", applet);
 	fd = open(file, O_WRONLY | O_CREAT | O_NONBLOCK, 0664);
+	free(file);
 	if (fd == -1)
 		return -1;
 	if (flock(fd, LOCK_EX | LOCK_NB) == -1) {
@@ -250,11 +252,12 @@ svc_lock(const char *applet)
 int
 svc_unlock(const char *applet, int fd)
 {
-	char file[PATH_MAX];
+	char *file = NULL;
 
-	snprintf(file, sizeof(file), RC_SVCDIR "/exclusive/%s", applet);
+	xasprintf(&file, RC_SVCDIR "/exclusive/%s", applet);
 	close(fd);
 	unlink(file);
+	free(file);
 	return -1;
 }
 
@@ -358,7 +361,7 @@ RC_DEPTREE * _rc_deptree_load(int force, int *regen)
 	int serrno = errno;
 	int merrno;
 	time_t t;
-	char file[PATH_MAX];
+	char *file = NULL;
 	struct stat st;
 	struct utimbuf ut;
 	FILE *fp;

commit b200c5a00949fdeef337e5828cfb8cd0378bca23

rc-misc.c: Allocate memory for 'file'
This is a partial revert of commit 8e02406d ("rc-misc.c: remove
references to PATH_MAX"), which changed 'file' to a null pointer with no
associated storage.

../openrc-0.44.10/src/rc/rc-misc.c: In function ‘_rc_deptree_load’:
../openrc-0.44.10/src/rc/rc-misc.c:392:33: warning: ‘%s’ directive argument is null [-Wformat-overflow=]
392 |                                 eerror("Clock skew detected with `%s'", file);
    |                                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Fixes: 8e02406d ("rc-misc.c: remove references to PATH_MAX")

src/rc/rc-misc.c
@@ -362,7 +362,7 @@ RC_DEPTREE * _rc_deptree_load(int force, int *regen)
 	int serrno = errno;
 	int merrno;
 	time_t t;
-	char *file = NULL;
+	char file[PATH_MAX];
 	struct stat st;
 	struct utimbuf ut;
 	FILE *fp;