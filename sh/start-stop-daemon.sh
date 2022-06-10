OpenRC/openrc

commit 6034866d1c74d5a23eb9f3e0ebf40c9d278aac93

Use consistent args passing in start-stop-daemon

sh/start-stop-daemon.sh
@@ -38,10 +38,6 @@ ssd_start()
 		service_inactive && _inactive=true
 		mark_service_inactive
 	fi
-	[ -n "$output_logger" ] &&
-		output_logger_arg="--stdout-logger \"$output_logger\""
-	[ -n "$error_logger" ] &&
-		error_logger_arg="--stderr-logger \"$error_logger\""
 	#the eval call is necessary for cases like:
 	# command_args="this \"is a\" test"
 	# to work properly.
@@ -51,8 +47,8 @@ ssd_start()
 		${directory:+--chdir} $directory \
 		${output_log+--stdout} $output_log \
 		${error_log+--stderr} $error_log \
-		${output_logger_arg} \
-		${error_logger_arg} \
+		${output_logger:+--stdout-logger} "$output_logger" \
+		${error_logger:+--stderr-logger} "$error_logger" \
 		${capabilities+--capabilities} "$capabilities" \
 		${secbits:+--secbits} "$secbits" \
 		${no_new_privs:+--no-new-privs} \

commit 83950bbe600e9fc91b322b9e39af4fe66072b75e

fix quoting of loggers in start-stop-daemon
previously broken in 6034866d1c74d5a23eb9f3e0ebf40c9d278aac93
caused *_logger options to be passed unquoted, so
`error_logger="logger -t .."` would pass -t to s-s-d and fail to start
the service.

sh/start-stop-daemon.sh
@@ -47,8 +47,8 @@ ssd_start()
 		${directory:+--chdir} $directory \
 		${output_log+--stdout} $output_log \
 		${error_log+--stderr} $error_log \
-		${output_logger:+--stdout-logger} "$output_logger" \
-		${error_logger:+--stderr-logger} "$error_logger" \
+		${output_logger:+--stdout-logger \"$output_logger\"} \
+		${error_logger:+--stderr-logger \"$error_logger\"} \
 		${capabilities+--capabilities} "$capabilities" \
 		${secbits:+--secbits} "$secbits" \
 		${no_new_privs:+--no-new-privs} \