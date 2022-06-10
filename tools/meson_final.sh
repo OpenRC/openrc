107b23819d6d6fa9ed05502599b2679606f2712f
William Hubbs <w.d.hubbs@gmail.com>
2022-06-10 10:28:45 -0500

    restore the init symlink for sysvinit mode
    This symlink got lost in the transition to meson.
    
    X-Gentoo-Bug: 850754
    X-Gentoo-Bug-URL: https://bugs.gentoo.org/850754

--- tools/meson_final.sh
+++ tools/meson_final.sh
@@ -4,10 +4,15 @@ set -e
 set -u
 
 rc_libexecdir="$1"
-os="$2"
+sbindir="$2"
+os="$3"
+sysvinit="$4"
 
-if [ ${os} != Linux ]; then
+if [ "${os}" != Linux ]; then
 	install -d "${DESTDIR}/${rc_libexecdir}"/init.d
 fi
 install -d "${DESTDIR}/${rc_libexecdir}"/tmp
 install -m 644 "${MESON_BUILD_ROOT}/src/shared/version" "${DESTDIR}/${rc_libexecdir}"
+if [ "${os}" = Linux ] && [ "${sysvinit}" = yes ]; then
+	ln -s openrc-init "${DESTDIR}/${sbindir}"/init
+fi
