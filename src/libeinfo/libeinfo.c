ff77e910cbdf082fcb2da535852ab49d3bfabc84
Sam James <sam@gentoo.org>
2023-01-29 04:18:42 +0000

    libeinfo: missing includes from IWYU

--- src/libeinfo/libeinfo.c
+++ src/libeinfo/libeinfo.c
@@ -15,16 +15,12 @@
  *    except according to the terms contained in the LICENSE file.
  */
 
-#include <sys/types.h>
 #include <sys/ioctl.h>
-#include <sys/stat.h>
-
 #include <errno.h>
-#include <fcntl.h>
 #include <inttypes.h>
-#include <limits.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
