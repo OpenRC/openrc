28ecb38515d6a8a52fa524a5fee8fda3e7cb8252
Sam James <sam@gentoo.org>
2023-01-29 04:16:01 +0000

    openrc-shutdown: missing includes from IWYU

--- src/openrc-shutdown/broadcast.c
+++ src/openrc-shutdown/broadcast.c
@@ -12,24 +12,23 @@
  * This file may not be copied, modified, propagated, or distributed
  *    except according to the terms contained in the LICENSE file.
  */
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/sysmacros.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <utmp.h>
-#include <utmpx.h>
-#include <pwd.h>
+
+
 #include <fcntl.h>
+#include <paths.h>
+#include <pwd.h>
 #include <signal.h>
 #include <setjmp.h>
-#include <paths.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <sys/types.h>
 #include <sys/utsname.h>
+#include <time.h>
+#include <unistd.h>
+#include <utmpx.h>
 
 #include "broadcast.h"
 #include "helpers.h"
