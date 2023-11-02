1433552435c6c2ad63be71b724b63be2917769ba
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2023-11-02 08:37:22 +0100

    sh/supervise-daemon.sh: Proper casing for --no-new-privs

--- sh/supervise-daemon.sh
+++ sh/supervise-daemon.sh
@@ -40,7 +40,7 @@ supervise_start()
 		${healthcheck_timer:+--healthcheck-timer} $healthcheck_timer \
 		${capabilities+--capabilities} "$capabilities" \
 		${secbits:+--secbits} "$secbits" \
-		${no_new_privs:+--no_new_privs} \
+		${no_new_privs:+--no-new-privs} \
 		${command_user+--user} $command_user \
 		${umask+--umask} $umask \
 		${supervise_daemon_args-${start_stop_daemon_args}} \
