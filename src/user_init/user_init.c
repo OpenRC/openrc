fd961d2ea0eea2cc60f4af1aca6b6e7711d43995
Anna (navi) Figueiredo Gomes <navi@vlhl.dev>
2024-07-12 00:51:12 +0200

    openrc-user: init script for users
    adds a new multiplexed script for starting user sessions. it also sets
    up XDG_RUNTIME_DIR.
    
    Signed-off-by: Anna (navi) Figueiredo Gomes <navi@vlhl.dev>

---
+++ src/user_init/user_init.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "rc.h"
+
+#define USERINIT RC_LIBEXECDIR "/sh/user-init.sh"
+
+int main(int argc, char **argv) {
+	struct passwd *user;
+	if (argc < 3)
+		return 1;
+
+	user = getpwnam(argv[1]);
+	if (!user || initgroups(user->pw_name, user->pw_gid) == -1
+			|| setgid(user->pw_gid) == -1
+			|| setuid(user->pw_uid) == -1)
+		return 1;
+
+	execl(user->pw_shell, "-", "-c", USERINIT, argv[2], NULL);
+}
