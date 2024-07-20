15de84d70ccd57afd7ad5af5cf95cef7fc542141
Anna (navi) Figueiredo Gomes <navi@vlhl.dev>
2024-07-20 15:16:00 +0200

    shared/plugin.c: fix incompatible function warning.
    Signed-off-by: Anna (navi) Figueiredo Gomes <navi@vlhl.dev>

--- src/shared/plugin.h
+++ src/shared/plugin.h
@@ -32,15 +32,4 @@ void rc_plugin_load(void);
 void rc_plugin_unload(void);
 void rc_plugin_run(RC_HOOK, const char *value);
 
-/* dlfunc defines needed to avoid ISO errors. FreeBSD has this right :) */
-#if !defined(__FreeBSD__) && !defined(__DragonFly__)
-struct __dlfunc_arg {
-	int	__dlfunc_dummy;
-};
-
-typedef	void (*dlfunc_t)(struct __dlfunc_arg);
-
-dlfunc_t dlfunc (void * __restrict handle, const char * __restrict symbol);
-#endif
-
 #endif
