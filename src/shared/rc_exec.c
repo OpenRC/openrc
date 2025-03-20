114ea265aa463476e379077b285a535eb59fbf1f
NRK <nrk@disroot.org>
2025-03-20 17:18:52 +0000

    rc_exec: add a centralized exec function
    currently there's a lot of ad-hoc fork exec going around. the
    goal of this is to centralize most of the typical cases.

---
+++ src/shared/rc_exec.c
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2025 The OpenRC Authors.
+ * See the Authors file at the top-level directory of this distribution and
+ * https://github.com/OpenRC/openrc/blob/HEAD/AUTHORS
+ *
+ * This file is part of OpenRC. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this
+ * distribution and at https://github.com/OpenRC/openrc/blob/HEAD/LICENSE
+ * This file may not be copied, modified, propagated, or distributed
+ * except according to the terms contained in the LICENSE file.
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "rc_exec.h"
+#include "helpers.h"
+
+static void checked_close(int fd)
+{
+	if (fd >= 0)
+		close(fd);
+}
+
+static int devnull(void)
+{
+	static int devnullfd = -1;
+	if (devnullfd < 0)
+		devnullfd = open("/dev/null", O_RDWR | O_CLOEXEC);
+	if (devnullfd < 0) {
+		fprintf(stderr, "Failed to open /dev/null: %s", strerror(errno));
+		_exit(1);
+	}
+	return devnullfd;
+}
+
+struct exec_args exec_init(const char **argv)
+{
+	struct exec_args args = {0};
+	args.argv = argv;
+	args.redirect_stdin  = EXEC_NO_REDIRECT;
+	args.redirect_stdout = EXEC_NO_REDIRECT;
+	args.redirect_stderr = EXEC_NO_REDIRECT;
+	return args;
+}
+
+struct exec_result do_exec(struct exec_args *args)
+{
+	struct exec_result res = { .pid = -1 };
+	sigset_t full, old;
+	int saved_errno, err, n;
+	int execpipe[2] = {-1, -1};
+	int stdin_pipe[2] = {-1, -1};
+	int stdout_pipe[2] = {-1, -1};
+	int stderr_pipe[2] = {-1, -1};
+
+	if (pipe2(execpipe, O_CLOEXEC) < 0)
+		goto exit;
+	if (args->redirect_stdin == EXEC_MKPIPE) {
+		if (pipe2(stdin_pipe, O_CLOEXEC) < 0)
+			goto exit;
+		args->redirect_stdin = stdin_pipe[0];
+	}
+	if (args->redirect_stdout == EXEC_MKPIPE) {
+		if (pipe2(stdout_pipe, O_CLOEXEC) < 0)
+			goto exit;
+		args->redirect_stdout = stdout_pipe[1];
+	}
+	if (args->redirect_stderr == EXEC_MKPIPE) {
+		if (pipe2(stderr_pipe, O_CLOEXEC) < 0)
+			goto exit;
+		args->redirect_stderr = stderr_pipe[1];
+	}
+
+	if (args->redirect_stdin == EXEC_DEVNULL)
+		args->redirect_stdin = devnull();
+	if (args->redirect_stdout == EXEC_DEVNULL)
+		args->redirect_stdout = devnull();
+	if (args->redirect_stderr == EXEC_DEVNULL)
+		args->redirect_stderr = devnull();
+
+	sigfillset(&full);
+	sigprocmask(SIG_SETMASK, &full, &old);
+	res.pid = fork();
+
+	if (res.pid < 0) {
+		sigprocmask(SIG_SETMASK, &old, NULL);
+		goto exit;
+	} else if (res.pid == 0) {
+		/* child */
+		if (args->redirect_stdin != EXEC_NO_REDIRECT &&
+		    dup2(args->redirect_stdin, STDIN_FILENO) < 0)
+			goto child_err;
+		if (args->redirect_stdout != EXEC_NO_REDIRECT &&
+		    dup2(args->redirect_stdout, STDOUT_FILENO) < 0)
+			goto child_err;
+		if (args->redirect_stderr != EXEC_NO_REDIRECT &&
+		    dup2(args->redirect_stderr, STDERR_FILENO) < 0)
+			goto child_err;
+		if (args->setsid && setsid() < 0)
+			goto child_err;
+		sigprocmask(SIG_SETMASK, &old, NULL);
+		execvp(args->argv[0], UNCONST(args->argv));
+child_err:
+		saved_errno = errno;
+		write(execpipe[1], &saved_errno, sizeof saved_errno);
+		_exit(1);
+	} else {
+		/* parent */
+		close(execpipe[1]);
+		execpipe[1] = -1;
+		n = read(execpipe[0], &err, sizeof err);
+		if (n != 0) {
+			/* exec failed, reap the child and cleanup */
+			waitpid(res.pid, NULL, 0); /* do we care about waitpid failures? */
+			res.pid = -1;
+		}
+		sigprocmask(SIG_SETMASK, &old, NULL);
+		if (res.pid < 0) {
+			errno = (n == sizeof err) ? err : EINVAL;
+			goto exit;
+		}
+
+		if (stdin_pipe[0] >= 0) {
+			res.proc_stdin = stdin_pipe[1];
+			stdin_pipe[1] = -1; /* to avoid closing it below */
+		}
+		if (stdout_pipe[0] >= 0) {
+			res.proc_stdout = stdout_pipe[0];
+			stdout_pipe[0] = -1;
+		}
+		if (stderr_pipe[0] >= 0) {
+			res.proc_stderr = stderr_pipe[0];
+			stderr_pipe[0] = -1;
+		}
+	}
+
+exit:
+	saved_errno = errno;
+	checked_close(execpipe[0]); checked_close(execpipe[1]);
+	checked_close(stdin_pipe[0]); checked_close(stdin_pipe[1]);
+	checked_close(stdout_pipe[0]); checked_close(stdout_pipe[1]);
+	checked_close(stderr_pipe[0]); checked_close(stderr_pipe[1]);
+	errno = saved_errno;
+	return res;
+}
---
+++ src/shared/rc_exec.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 The OpenRC Authors.
+ * See the Authors file at the top-level directory of this distribution and
+ * https://github.com/OpenRC/openrc/blob/HEAD/AUTHORS
+ *
+ * This file is part of OpenRC. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this
+ * distribution and at https://github.com/OpenRC/openrc/blob/HEAD/LICENSE
+ * This file may not be copied, modified, propagated, or distributed
+ * except according to the terms contained in the LICENSE file.
+ */
+#ifndef RC_EXEC_H
+#define RC_EXEC_H
+
+#include <stdbool.h>
+#include <sys/types.h>
+
+/*
+ * goal of this api is to provide a convenient and centralized way to do most
+ * of the fork() + exec() work. it doesn't attempt to do everything, but simple
+ * use-cases should be covered. if any simple functionality is missing, add it
+ * here instead of ad-hoc coding fork+exec yourself.
+ */
+
+enum {
+	/* default */
+	EXEC_NO_REDIRECT = -1,
+	/* make a pipe and redirect to it */
+	EXEC_MKPIPE = -2,
+	/* redirect to /dev/null */
+	EXEC_DEVNULL = -3,
+};
+
+struct exec_args {
+	const char **argv;
+	int redirect_stdin;
+	int redirect_stdout;
+	int redirect_stderr;
+	bool setsid : 1;
+};
+
+struct exec_result {
+	pid_t pid;
+	/* returned in case of RC_EXEC_MKPIPE */
+	int proc_stdin;
+	int proc_stdout;
+	int proc_stderr;
+};
+
+struct exec_args exec_init(const char **argv);
+struct exec_result do_exec(struct exec_args *args);
+
+#endif
