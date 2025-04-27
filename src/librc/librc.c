OpenRC/openrc

commit 27eae2c380239e7baf113c1b228c6fe679e6a541

librc: clear dirfds when switching to user mode

src/librc/librc.c
@@ -636,6 +636,13 @@ rc_set_user(void)
 
 	rc_dirs.scriptdirs[SCRIPTDIR_USR] = rc_dirs.usrconfdir;
 	rc_dirs.scriptdirs[SCRIPTDIR_SVC] = rc_dirs.svcdir;
+
+	for (size_t i = 0; i < RC_DIR_MAX; i++) {
+		if (dirfds[i] == -1)
+			continue;
+		close(dirfds[i]);
+		dirfds[i] = -1;
+	}
 }
 
 const char * const *

commit cf27c72bb4d992f8092cb0c82cd5602659692bc6

librc: dirfd is initalized with zero not with -1
Without this patch, this code therefore closes FD 0 repeatedly.

See https://github.com/OpenRC/openrc/pull/851

This fixes a regression from 27eae2c380239e7baf113c1b228c6fe679e6a541.

src/librc/librc.c
@@ -638,8 +638,11 @@ rc_set_user(void)
 	rc_dirs.scriptdirs[SCRIPTDIR_SVC] = rc_dirs.svcdir;
 
 	for (size_t i = 0; i < RC_DIR_MAX; i++) {
-		if (dirfds[i] == -1)
+		/* dirfd is zero-initialized, though zero is a valid
+		 * FD, dirfd should never contain FDs which are <= 2. */
+		if (dirfds[i] <= 0)
 			continue;
+
 		close(dirfds[i]);
 		dirfds[i] = -1;
 	}