OpenRC/openrc

commit 07d6f4d526dbf9f3b6720727544e5d0037c89853

librc: attempt to create svcdirs on demand
we would no longer need to create runtime directories during the
(conditional) dependency update needed check, so we can remove the
checks from there and instead rely on the directory apis

src/librc/librc-depend.c
@@ -689,17 +689,6 @@ rc_deptree_update_needed(time_t *newest, char *file)
 	RC_STRING *s;
 	struct stat buf;
 	time_t mtime;
-	char *path;
-	const char *service_dir = rc_svcdir();
-
-	/* Create base directories if needed */
-	if (mkdir(service_dir, 0755) != 0 && errno != EEXIST)
-		fprintf(stderr, "mkdir '%s': %s\n", service_dir, strerror(errno));
-
-	for (size_t i = 1; i < RC_DIR_SYS_MAX; i++) {
-		if (mkdirat(rc_dirfd(RC_DIR_SVCDIR), dirnames[i], 0755) != 0 && errno != EEXIST)
-			fprintf(stderr, "mkdir `%s': %s\n", dirnames[i], strerror(errno));
-	}
 
 	/* Quick test to see if anything we use has changed and we have
 	 * data in our deptree. */

src/librc/librc.c
@@ -50,28 +50,35 @@ static int dirfds[RC_DIR_MAX];
 
 int rc_dirfd(enum rc_dir dir) {
 	int flags = O_RDONLY | O_CLOEXEC | O_DIRECTORY;
+	int dirfd = AT_FDCWD;
+	const char *dirname;
+
 	if (dir >= RC_DIR_MAX)
 		return -1;
 	if (dirfds[dir] <= 0) {
 		switch (dir) {
 		case RC_DIR_INVALID:
 			return -1;
-		case RC_DIR_SVCDIR:
-			dirfds[dir] = open(rc_svcdir(), flags);
-			break;
-		case RC_DIR_RUNLEVEL:
-			dirfds[dir] = open(rc_runleveldir(), flags);
-			break;
 		case RC_DIR_SYSCONF:
-			dirfds[dir] = open(rc_sysconfdir(), flags);
-			break;
+			return dirfds[dir] = open(rc_sysconfdir(), flags);
 		case RC_DIR_USRCONF:
-			dirfds[dir] = open(rc_usrconfdir(), flags);
+			return dirfds[dir] = open(rc_usrconfdir(), flags);
+		case RC_DIR_RUNLEVEL:
+			return dirfds[dir] = open(rc_runleveldir(), flags);
+		case RC_DIR_SVCDIR:
+			dirname = rc_svcdir();
 			break;
 		default:
 			assert(dirnames[dir]);
-			dirfds[dir] = openat(rc_dirfd(RC_DIR_SVCDIR), dirnames[dir], flags);
+			dirname = dirnames[dir];
+			dirfd = rc_dirfd(RC_DIR_SVCDIR);
 			break;
+		}
+
+		if ((dirfds[dir] = openat(dirfd, dirname, flags)) == -1 && errno == ENOENT) {
+			if (mkdirat(dirfd, dirname, 0755) == -1 && errno != EEXIST)
+				return -1;
+			dirfds[dir] = openat(dirfd, dirname, flags);
 		}
 	}
 	return dirfds[dir];
@@ -665,9 +672,9 @@ rc_runleveldir(void)
 const char *
 rc_svcdir(void)
 {
-	if (rc_dirs.set)
-		return rc_dirs.svcdir;
-	return RC_SVCDIR;
+	const char *path = rc_dirs.set ? rc_dirs.svcdir : RC_SVCDIR;
+	mkdir(path, 0755);
+	return path;
 }
 
 static ssize_t

commit 43571e8a24e72911e3fa459c525e349ef60222de

librc: don't create /run/openrc itself on demand.
this is a temporary fix, in a system that boots with / as rw, /run
unmounted, and has rc_parallel on, openrc would create a dirfd to
/run/openrc before mounting /run, thus keeping an open fd to the old
/run mountpoint, and creating 'exclusive' lockfiles under it, that
subsequent services can't see nor lock on.

Fixes: https://github.com/OpenRC/openrc/issues/859
Fixes: 07d6f4d5

src/librc/librc-depend.c
@@ -693,6 +693,8 @@ rc_deptree_update_needed(time_t *newest, char *file)
 
 	/* Quick test to see if anything we use has changed and we have
 	 * data in our deptree. */
+	if (mkdir(rc_svcdir(), 0755) != 0 && errno != EEXIST)
+		fprintf(stderr, "mkdir '%s': %s\n", rc_svcdir(), strerror(errno));
 
 	if (fstatat(rc_dirfd(RC_DIR_SVCDIR), "deptree", &buf, 0) == 0) {
 		mtime = buf.st_mtime;

src/librc/librc.c
@@ -50,8 +50,7 @@ static int dirfds[RC_DIR_MAX];
 
 int rc_dirfd(enum rc_dir dir) {
 	int flags = O_RDONLY | O_CLOEXEC | O_DIRECTORY;
-	int dirfd = AT_FDCWD;
-	const char *dirname;
+	int dirfd;
 
 	if (dir >= RC_DIR_MAX)
 		return -1;
@@ -66,19 +65,16 @@ int rc_dirfd(enum rc_dir dir) {
 		case RC_DIR_RUNLEVEL:
 			return dirfds[dir] = open(rc_runleveldir(), flags);
 		case RC_DIR_SVCDIR:
-			dirname = rc_svcdir();
-			break;
+			return dirfds[dir] = open(rc_svcdir(), flags);
 		default:
 			assert(dirnames[dir]);
-			dirname = dirnames[dir];
 			dirfd = rc_dirfd(RC_DIR_SVCDIR);
+			if ((dirfds[dir] = openat(dirfd, dirnames[dir], flags)) == -1 && errno == ENOENT) {
+				if (mkdirat(dirfd, dirnames[dir], 0755) == -1 && errno != EEXIST)
+					return -1;
+				dirfds[dir] = openat(dirfd, dirnames[dir], flags);
+			}
 			break;
-		}
-
-		if ((dirfds[dir] = openat(dirfd, dirname, flags)) == -1 && errno == ENOENT) {
-			if (mkdirat(dirfd, dirname, 0755) == -1 && errno != EEXIST)
-				return -1;
-			dirfds[dir] = openat(dirfd, dirname, flags);
 		}
 	}
 	return dirfds[dir];
@@ -704,9 +700,7 @@ rc_runleveldir(void)
 const char *
 rc_svcdir(void)
 {
-	const char *path = rc_dirs.set ? rc_dirs.svcdir : RC_SVCDIR;
-	mkdir(path, 0755);
-	return path;
+	return rc_dirs.set ? rc_dirs.svcdir : RC_SVCDIR;
 }
 
 static ssize_t