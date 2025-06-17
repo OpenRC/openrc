OpenRC/openrc

commit 7208da23b1762c5b3b747ad7b714906ede9682ab

*-daemon: filter all RC_ and SSD_ variables for the service
there's no real reason to leak those into the environment.

Closes: https://github.com/OpenRC/openrc/issues/808

src/start-stop-daemon/start-stop-daemon.c
@@ -1033,17 +1033,9 @@ int main(int argc, char **argv)
 #endif
 
 		TAILQ_FOREACH(env, env_list, entries) {
-			if ((strncmp(env->value, "RC_", 3) == 0 &&
-				strncmp(env->value, "RC_SERVICE=", 11) != 0 &&
-				strncmp(env->value, "RC_SVCNAME=", 11) != 0) ||
-				strncmp(env->value, "SSD_NICELEVEL=", 14) == 0 ||
-				strncmp(env->value, "SSD_IONICELEVEL=", 16) == 0 ||
-				strncmp(env->value, "SSD_OOM_SCORE_ADJ=", 18) == 0)
-			{
-				p = strchr(env->value, '=');
-				*p = '\0';
+			if (strncmp(env->value, "RC_", 3) == 0 && strncmp(env->value, "SSD_", 4) == 0) {
+				*strchr(env->value, '=') = '\0';
 				unsetenv(env->value);
-				continue;
 			}
 		}
 		rc_stringlist_free(env_list);

src/supervise-daemon/supervise-daemon.c
@@ -511,17 +511,9 @@ RC_NORETURN static void child_process(char *exec, char **argv)
 #endif
 
 	TAILQ_FOREACH(env, env_list, entries) {
-		if ((strncmp(env->value, "RC_", 3) == 0 &&
-			strncmp(env->value, "RC_SERVICE=", 11) != 0 &&
-			strncmp(env->value, "RC_SVCNAME=", 11) != 0) ||
-		    strncmp(env->value, "SSD_NICELEVEL=", 14) == 0 ||
-		    strncmp(env->value, "SSD_IONICELEVEL=", 16) == 0 ||
-		    strncmp(env->value, "SSD_OOM_SCORE_ADJ=", 18) == 0)
-		{
-			p = strchr(env->value, '=');
-			*p = '\0';
+		if (strncmp(env->value, "RC_", 3) == 0 && strncmp(env->value, "SSD_", 4) == 0) {
+			*strchr(env->value, '=') = '\0';
 			unsetenv(env->value);
-			continue;
 		}
 	}
 	rc_stringlist_free(env_list);

commit 491f4631f5d34ababce838dd3827cc15ca38e682

*-daemon: actually filter RC_/SSD_ variables
oops, inverted check

Fixes: 7208da2

src/start-stop-daemon/start-stop-daemon.c
@@ -1045,7 +1045,7 @@ int main(int argc, char **argv)
 #endif
 
 		TAILQ_FOREACH(env, env_list, entries) {
-			if (strncmp(env->value, "RC_", 3) == 0 && strncmp(env->value, "SSD_", 4) == 0) {
+			if (strncmp(env->value, "RC_", 3) == 0 || strncmp(env->value, "SSD_", 4) == 0) {
 				*strchr(env->value, '=') = '\0';
 				unsetenv(env->value);
 			}

src/supervise-daemon/supervise-daemon.c
@@ -526,7 +526,7 @@ RC_NORETURN static void child_process(char *exec, char **argv)
 #endif
 
 	TAILQ_FOREACH(env, env_list, entries) {
-		if (strncmp(env->value, "RC_", 3) == 0 && strncmp(env->value, "SSD_", 4) == 0) {
+		if (strncmp(env->value, "RC_", 3) == 0 || strncmp(env->value, "SSD_", 4) == 0) {
 			*strchr(env->value, '=') = '\0';
 			unsetenv(env->value);
 		}