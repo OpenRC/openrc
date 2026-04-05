OpenRC/openrc

commit b6db87756c9ae6338bd58c02bdca941adf6db97d

*-daemon: open the redirect files in nonblocking mode
otherwise, we can get stuck if these files are fifos.

there might be cases where the service inheriting them in
nonblock mode also is desired, but that'd require exposing
options for it in the init script so leave that out for now.

Closes: https://github.com/OpenRC/openrc/issues/982

src/shared/helpers.h
@@ -29,6 +29,7 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define ERRX do { fprintf (stderr, "out of memory\n"); exit (1); } while (0)
 
@@ -282,6 +283,18 @@ RC_UNUSED static char *env_findvar(const char *name, char **envp)
 		if (strncmp(name, *var, len) == 0 && (*var)[len] == '=')
 			return *var;
 	return NULL;
+}
+
+RC_UNUSED static int xopen_nonblock(const char *filename, int flags, mode_t mode)
+{
+	int fd = open(filename, flags | O_NONBLOCK, mode);
+	if (fd >= 0 && fcntl(F_SETFL, fd, flags & ~O_NONBLOCK) < 0) {
+		int saved_errno = errno;
+		close(fd);
+		fd = -1;
+		errno = saved_errno;
+	}
+	return fd;
 }
 
 #endif

src/start-stop-daemon/start-stop-daemon.c
@@ -1086,17 +1086,17 @@ int main(int argc, char **argv)
 		stdout_fd = devnull_fd;
 		stderr_fd = devnull_fd;
 		if (redirect_stdin) {
-			if ((stdin_fd = open(redirect_stdin,
-				    O_RDONLY)) == -1)
-				eerrorx("%s: unable to open the input file"
+			if ((stdin_fd = xopen_nonblock(redirect_stdin,
+				    O_RDONLY, 0)) == -1)
+				eerrorx("%s: unable to redirect the input file"
 				    " for stdin `%s': %s",
 				    applet, redirect_stdin, strerror(errno));
 		}
 		if (redirect_stdout) {
-			if ((stdout_fd = open(redirect_stdout,
+			if ((stdout_fd = xopen_nonblock(redirect_stdout,
 				    O_WRONLY | O_CREAT | O_APPEND,
 				    S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)) == -1)
-				eerrorx("%s: unable to open the logfile"
+				eerrorx("%s: unable to redirect the logfile"
 				    " for stdout `%s': %s",
 				    applet, redirect_stdout, strerror(errno));
 		}else if (stdout_process) {
@@ -1107,10 +1107,10 @@ int main(int argc, char **argv)
 				    applet, stdout_process, strerror(errno));
 		}
 		if (redirect_stderr) {
-			if ((stderr_fd = open(redirect_stderr,
+			if ((stderr_fd = xopen_nonblock(redirect_stderr,
 				    O_WRONLY | O_CREAT | O_APPEND,
 				    S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)) == -1)
-				eerrorx("%s: unable to open the logfile"
+				eerrorx("%s: unable to redirect the logfile"
 				    " for stderr `%s': %s",
 				    applet, redirect_stderr, strerror(errno));
 		}else if (stderr_process) {

src/supervise-daemon/supervise-daemon.c
@@ -567,17 +567,16 @@ RC_NORETURN static void child_process(char *exec, char **argv)
 	stdout_fd = devnull_fd;
 	stderr_fd = devnull_fd;
 	if (redirect_stdin) {
-		if ((stdin_fd = open(redirect_stdin,
-			    O_RDONLY)) == -1)
-			eerrorx("%s: unable to open the input file"
+		if ((stdin_fd = xopen_nonblock(redirect_stdin, O_RDONLY, 0)) == -1)
+			eerrorx("%s: unable to redirect the input file"
 			    " for stdin `%s': %s",
 			    applet, redirect_stdin, strerror(errno));
 	}
 	if (redirect_stdout) {
-		if ((stdout_fd = open(redirect_stdout,
+		if ((stdout_fd = xopen_nonblock(redirect_stdout,
 			    O_WRONLY | O_CREAT | O_APPEND,
 			    S_IRUSR | S_IWUSR)) == -1)
-			eerrorx("%s: unable to open the logfile"
+			eerrorx("%s: unable to redirect the logfile"
 				    " for stdout `%s': %s",
 				    applet, redirect_stdout, strerror(errno));
 	} else if (stdout_process) {
@@ -588,10 +587,10 @@ RC_NORETURN static void child_process(char *exec, char **argv)
 			    applet, stdout_process, strerror(errno));
 	}
 	if (redirect_stderr) {
-		if ((stderr_fd = open(redirect_stderr,
+		if ((stderr_fd = xopen_nonblock(redirect_stderr,
 			    O_WRONLY | O_CREAT | O_APPEND,
 			    S_IRUSR | S_IWUSR)) == -1)
-			eerrorx("%s: unable to open the logfile"
+			eerrorx("%s: unable to redirect the logfile"
 			    " for stderr `%s': %s",
 			    applet, redirect_stderr, strerror(errno));
 	} else if (stderr_process) {

commit c1e92f54cb212cb06a0ec983b61f85992d594cca

shared: fix fcntl argument order
how this managed to survive my tests is beyond me...

fixes: b6db87756c9ae6338bd58c02bdca941adf6db97d

src/shared/helpers.h
@@ -288,7 +288,7 @@ RC_UNUSED static char *env_findvar(const char *name, char **envp)
 RC_UNUSED static int xopen_nonblock(const char *filename, int flags, mode_t mode)
 {
 	int fd = open(filename, flags | O_NONBLOCK, mode);
-	if (fd >= 0 && fcntl(F_SETFL, fd, flags & ~O_NONBLOCK) < 0) {
+	if (fd >= 0 && fcntl(fd, F_SETFL, flags & ~O_NONBLOCK) < 0) {
 		int saved_errno = errno;
 		close(fd);
 		fd = -1;