bc44ab3f590eba30e118f61b6a375b56db1d3401
Roman Kiselevich <roman.kiselevich.dev@gmail.com>
2026-05-23 21:25:08 +0200

    tools: fix lowercase linux string

--- tools/meson_final.sh
+++ tools/meson_final.sh
@@ -6,7 +6,7 @@ set -u
 rc_libexecdir="$1"
 os="$2"
 
-if [ "${os}" != Linux ]; then
+if [ "${os}" != linux ]; then
 	install -d "${DESTDIR}/${rc_libexecdir}"/init.d
 fi
 install -m 644 "${MESON_BUILD_ROOT}/src/shared/version" "${DESTDIR}/${rc_libexecdir}"
