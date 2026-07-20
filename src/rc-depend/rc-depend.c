f4be97d145fc003b7c0315cabef5464cca0758b2
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2026-07-21 01:37:46 +0200

    Set default runlevel to 'default'
    The existing default runlevel of sysinit was never intentional, and a
    side-effect of how system mode works.
    
    Closes: https://github.com/OpenRC/openrc/issues/1035

--- src/rc-depend/rc-depend.c
+++ src/rc-depend/rc-depend.c
@@ -112,7 +112,7 @@ int main(int argc, char **argv)
 	}
 
 	if (!runlevel)
-		runlevel = rc_runlevel_get();
+		runlevel = default_runlevel();
 
 	services = rc_stringlist_new();
 	while (optind < argc) {
