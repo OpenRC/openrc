OpenRC/openrc

commit 53cf9bc7d3a997afdf79929c69ffdd0b7c7d79b3

sysv-initctl: open write end of fifo to prevent busylooping
and unify logic with openrc-init into a helper

Closes: https://github.com/OpenRC/openrc/pull/1056

src/shared/helpers.h
@@ -259,4 +259,42 @@ RC_UNUSED static int xopen_nonblock(const char *filename, int flags, mode_t mode
 	return fd;
 }
 
+RC_UNUSED static int open_rwfifo(const char *path, bool nonblock)
+{
+	int r, w, err;
+
+	if (mkfifo(path, 0600) == -1 && errno != EEXIST)
+		return -1;
+
+	if ((r = open(path, O_RDONLY | O_NONBLOCK | O_CLOEXEC)) == -1)
+		return -1;
+
+	/* Leak an open writeable fd for the life of the process
+	 * to keep the read end valid after a client disconnects.
+	 * On linux, opening O_RDWR would solve this, but posix
+	 * leaves that behaviour undefined, and for example, hurd
+	 * does not properly handle it. */
+	if ((w = open(path, O_WRONLY | O_NONBLOCK | O_CLOEXEC) == -1)) {
+		err = errno;
+		goto r_err;
+	}
+
+	if (!nonblock) {
+		int flags = fcntl(r, F_GETFL);
+		if (flags == -1 || fcntl(r, flags & ~O_NONBLOCK) == -1) {
+			err = errno;
+			goto w_err;
+		}
+	}
+
+	return r;
+
+w_err:
+	close(w);
+r_err:
+	close(r);
+	errno = err;
+	return -1;
+}
+
 #endif

src/openrc-init/openrc-init.c
@@ -283,34 +283,6 @@ static void reap_zombies(int sig RC_UNUSED)
 	errno = saved_errno;
 }
 
-static int open_fifo(const char *path)
-{
-	int dummy_writer;
-	int reader;
-	int saved_errno;
-
-	if (mkfifo(path, 0600) == -1 && errno != EEXIST)
-		return -1;
-
-	reader = open(path, O_RDONLY | O_NONBLOCK | O_CLOEXEC);
-	if (reader == -1)
-		return -1;
-
-	/*
-	 * Keep the dummy writer open for the lifetime of the process so that the
-	 * read end does not continuously report POLLHUP when a client disconnects.
-	 */
-	dummy_writer = open(path, O_WRONLY | O_NONBLOCK | O_CLOEXEC);
-	if (dummy_writer == -1) {
-		saved_errno = errno;
-		close(reader);
-		errno = saved_errno;
-		return -1;
-	}
-
-	return reader;
-}
-
 int main(int argc, char **argv)
 {
 	char *cmdline_runlevel = NULL;
@@ -395,9 +367,8 @@ int main(int argc, char **argv)
 	if (!reexec)
 		init(cmdline_runlevel);
 
-	fifo = open_fifo(RC_INIT_FIFO);
-	if (fifo == -1)
-		perror("open_fifo");
+	if ((fifo = open_rwfifo(RC_INIT_FIFO, true)) == -1)
+		perror("open_rwfifo");
 
 	for (;;) {
 		enum { FD_FIFO, FD_SIG, FD_COUNT };

src/sysv-initctl/initctl.c
@@ -11,6 +11,7 @@
 #include <rc.h>
 
 #include "initreq.h"
+#include "helpers.h"
 
 static bool init_halt = false;
 
@@ -73,13 +74,9 @@ int main(void) {
 	int fifo;
 
 	openlog("sysv-initctl", 0, LOG_DAEMON);
-	if (mkfifo("/run/initctl", 0600) == -1 && errno != EEXIST) {
-		syslog(LOG_ERR, "mkfifo: %s", strerror(errno));
-		return 1;
-	}
 	symlink("/run/initctl", "/dev/initctl");
-	if ((fifo = open("/run/initctl", O_RDONLY | O_NONBLOCK)) == -1) {
-		syslog(LOG_ERR, "open: %s", strerror(errno));
+	if ((fifo = open_rwfifo("/run/initctl", false)) == -1) {
+		syslog(LOG_ERR, "open_rwfifo: %s", strerror(errno));
 		return 1;
 	}
 

commit 245cda6ed0a7fe506322ca1ec84c38d7a4b1871a

shared: fix open_rwfifo descriptor handling
Fix operator precedence when opening the dummy writer, and pass
F_SETFL when clearing O_NONBLOCK on the read descriptor.

Basically, typos introduced in 53cf9bc7d.

src/shared/helpers.h
@@ -274,14 +274,14 @@ RC_UNUSED static int open_rwfifo(const char *path, bool nonblock)
 	 * On linux, opening O_RDWR would solve this, but posix
 	 * leaves that behaviour undefined, and for example, hurd
 	 * does not properly handle it. */
-	if ((w = open(path, O_WRONLY | O_NONBLOCK | O_CLOEXEC) == -1)) {
+	if ((w = open(path, O_WRONLY | O_NONBLOCK | O_CLOEXEC)) == -1) {
 		err = errno;
 		goto r_err;
 	}
 
 	if (!nonblock) {
 		int flags = fcntl(r, F_GETFL);
-		if (flags == -1 || fcntl(r, flags & ~O_NONBLOCK) == -1) {
+		if (flags == -1 || fcntl(r, F_SETFL, flags & ~O_NONBLOCK) == -1) {
 			err = errno;
 			goto w_err;
 		}